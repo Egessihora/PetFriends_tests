05824707308c04c6c6d7d8a204ee6cf8def7d0d0
Anastasiya Begunova <123498835+Egessihora@users.noreply.github.com>
2023-04-02 16:48:07 +0300

    Add tests

---
+++ test_pet_friends_main_page.py
@@ -0,0 +1,55 @@
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from autorisation import petfriends_login
+
+
+# проверка удачной авторизации
+def test_petfriends_login(browser):
+    result = petfriends_login(browser)
+    assert result == "PetFriends", "login error"
+
+
+# проверка элементов карточек питомцев (фото, имя, описание)
+def test_petfriends_pets_cards(browser):
+    test_petfriends_login(browser)
+    images = browser.find_elements(By.CSS_SELECTOR, '.card-deck .card-img-top')
+    names = browser.find_elements(By.CSS_SELECTOR, '.card-deck .card-title')
+    descriptions = browser.find_elements(By.CSS_SELECTOR, '.card-deck .card-text')
+
+    for i in range(len(names)):
+        assert images[i].get_attribute('src') != '', 'Image not found'
+        assert names[i].text != '', 'Name not found'
+        assert descriptions[i].text != '', 'Description not found'
+        parts = descriptions[i].text.split(", ")
+        assert len(parts[0]) > 0, 'Species not found'
+        assert len(parts[1]) > 0, 'Age not found'
+
+
+# Проверка элементов страницы
+def test_petfriends_card_deck(browser):
+    test_petfriends_login(browser)
+    # наличие навигационной панели
+    WebDriverWait(browser, 10).until(  # явное ожидание
+        EC.presence_of_element_located((By.ID, 'navbarNav')))
+    # наличие кнопки "PetFriends"
+    WebDriverWait(browser, 10).until(
+        EC.visibility_of_element_located((By.CSS_SELECTOR, 'a.navbar-brand.header2')))
+    # наличие кнопки "Мои питомцы"
+    WebDriverWait(browser, 10).until(
+        EC.visibility_of_element_located((By.CSS_SELECTOR, 'a[href="/my_pets"]')))
+    # наличие кнопки "Все питомцы"
+    WebDriverWait(browser, 10).until(
+        EC.visibility_of_element_located((By.CSS_SELECTOR, 'a[href="/all_pets"]')))
+    # наличие кнопки "Выйти"
+    WebDriverWait(browser, 10).until(
+        EC.visibility_of_element_located((By.CSS_SELECTOR, 'button.btn.btn-outline-secondary')))
+    # наличие центральной надписи "PetFriends"
+    WebDriverWait(browser, 10).until(
+        EC.visibility_of_element_located((By.CSS_SELECTOR, 'h1.text-center')))
+    # наличие центральной надписи "Все питомцы наших пользователей"
+    WebDriverWait(browser, 10).until(
+        EC.visibility_of_element_located((By.CSS_SELECTOR, 'div.text-center:nth-child(2)')))
+    # наличие таблицы с питомцами
+    WebDriverWait(browser, 10).until(
+        EC.presence_of_element_located((By.CSS_SELECTOR, '.card-deck')))
